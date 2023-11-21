dc33be5eb1c731b4260cd1c74f55f18a010da259
HasnaAkter <hasnaakter.cse@gmail.com>
2023-11-21 22:01:14 +0600

    popup

--- src/components/HomePageCard/PhysiotherapistCard.jsx
+++ src/components/HomePageCard/PhysiotherapistCard.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
- import '../../components/HomePageCard/Physio.css'
+import React from "react";
+import "../../components/HomePageCard/Physio.css";
 
-const PhysiotherapistCard = ({ name, title, location, availability, cost, profileUrl }) => {
+const PhysiotherapistCard = ({
+  name,
+  title,
+  location,
+  availability,
+  cost,
+  urlimg,
+  profileUrl,
+  employDegicnation,
+  employDetiels,
+  employName,
+}) => {
   return (
     // <div className="physiotherapist-card">
     //   <img src={profileUrl} alt={name} className="card-image" />
@@ -16,23 +27,42 @@ const PhysiotherapistCard = ({ name, title, location, availability, cost, profil
     // </div>
 
     <div className="max-w-sm rounded overflow-hidden shadow-lg physiotherapist-card">
-  <img src={profileUrl} alt={name} classNameName="card-image" />
-  <div className="px-6 py-4">
-    <div className="font-bold text-xl mb-2">{name}</div>
-    <p className="text-gray-700 text-base">
-    {title}
-    </p>
-  </div>
-  <div className="px-6 pt-4 pb-2">
-    <span className="inline-block  rounded-full  text-sm font-semibold text-gray-700 mr-2 mb-2">{location}</span>
-    <span className="inline-block rounded-full  text-sm font-semibold text-gray-700 mr-2 mb-2">${cost}</span>
-    
-    
-  </div>
- <div className='px-6 pt-4 pb-2'>
- <button className='btn btn-outline btn-warning '>View Profile</button>
- </div>
-</div>
+      <img src={profileUrl} alt={name} className="card-image" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <p className="text-gray-700 text-base">{title}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block  rounded-full  text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {location}
+        </span>
+        <span className="inline-block rounded-full  text-sm font-semibold text-gray-700 mr-2 mb-2">
+          ${cost}
+        </span>
+      </div>
+      <div className="px-6  pb-2">
+        <button
+          className="btn btn-outline btn-warning  "
+          onClick={() => document.getElementById("my_modal_1").showModal()}
+        >
+          View Profile
+        </button>
+        <dialog id="my_modal_1" className="modal" aria-labelledby="modal-label">
+          <div className="modal-box">
+          <img src={urlimg} alt={employName} className="card-image" />
+            <h3 className="font-bold text-lg">{employName}</h3>
+            <h3 className="font-medium text-lg">{employDegicnation}</h3>
+            <p className="py-4">{employDetiels}</p>
+
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
+    </div>
   );
 };
 
--- src/pages/HomePages/PhysiotherapistCardList.jsx
+++ src/pages/HomePages/PhysiotherapistCardList.jsx
@@ -1,36 +1,50 @@
-import React from 'react';
-import PhysiotherapistCard from '../../components/HomePageCard/PhysiotherapistCard';
+import React from "react";
+import PhysiotherapistCard from "../../components/HomePageCard/PhysiotherapistCard";
 import dc1 from "../../assets/Homepages/dc1.jpg";
 import dc2 from "../../assets/Homepages/dc2.png";
 import dc3 from "../../assets/Homepages/dc3.png";
 
+
 const PhysiotherapistCardList = () => {
   const physiotherapists = [
     {
-      name: 'Karyen Anderson',
-      title: 'BTP - Senior Physiotherapist',
-      location: 'Dhanmondi, Dhaka, Bangladesh',
-      availability: 'Mon, 22 December',
-      cost: '$15',
+      name: "Karyen Anderson",
+      title: "BTP - Senior Physiotherapist",
+      location: "Dhanmondi, Dhaka, Bangladesh",
+      availability: "Mon, 22 December",
+      cost: "$15",
       profileUrl: [dc1],
+      employName: "Karyen Anderson",
+      employDegicnation: "BTP - Senior Physiotherapist",
+      employDetiels:
+        "I am dedicated to helping individuals regain their mobility and improve their quality of life. With a strong background in orthopedic and sports injuries, I specialize in creating personalized rehabilitation plans. My goal is to empower patients to achieve their optimal physical health.",
     },
     {
-        name: 'Karyen Anderson',
-        title: 'BTP - Senior Physiotherapist',
-        location: 'Dhanmondi, Dhaka, Bangladesh',
-        availability: 'Mon, 22 December',
-        cost: '$15',
-        profileUrl: [dc3],
-      },
-      {
-        name: 'Karyen Anderson',
-        title: 'BTP - Senior Physiotherapist',
-        location: 'Dhanmondi, Dhaka, Bangladesh',
-        availability: 'Mon, 22 December',
-        cost: '$15',
-        profileUrl: [dc2],
-      },
-    
+      name: "Karyen Anderson",
+      title: "BTP - Senior Physiotherapist",
+      location: "Dhanmondi, Dhaka, Bangladesh",
+      availability: "Mon, 22 December",
+      cost: "$15",
+      profileUrl: [dc3],
+      employName: "Karyen Anderson",
+      employDegicnation: "BTP - Senior Physiotherapist",
+      employDetiels:
+        "I am dedicated to helping individuals regain their mobility and improve their quality of life. With a strong background in orthopedic and sports injuries, I specialize in creating personalized rehabilitation plans. My goal is to empower patients to achieve their optimal physical health.",
+    },
+    {
+      name: "Karyen Anderson",
+      title: "BTP - Senior Physiotherapist",
+      location: "Dhanmondi, Dhaka, Bangladesh",
+      availability: "Mon, 22 December",
+      cost: "$15",
+      profileUrl: [dc2],
+      urlimg:[dc2],
+      employName: "Karyen Anderson",
+      employDegicnation: "BTP - Senior Physiotherapist",
+      employDetiels:
+        "I am dedicated to helping individuals regain their mobility and improve their quality of life. With a strong background in orthopedic and sports injuries, I specialize in creating personalized rehabilitation plans. My goal is to empower patients to achieve their optimal physical health.",
+      
+    },
   ];
 
   return (
